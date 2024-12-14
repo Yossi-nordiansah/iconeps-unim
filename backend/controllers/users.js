9146a92c61eb623049b550cac1be504e1bbe568e
Yossi-nordiansah <yossi.nordiansah99@gmail.com>
2024-12-14 23:37:18 +0700

    membuat access token dan refresh token

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,5 +1,6 @@
 import prismaClient from "../src/prisma-client.js";
 import bycript from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const getUsers = async (req, res) => {
     try {
@@ -41,5 +42,31 @@ export const Register = async (req, res) => {
     } catch (error) {
         res.status(500).send({message : 'Registrasi Gagal'})
         console.log(error)
+    }
+}
+
+export const Login = async (req, res) => {
+    try {
+        const user = await prismaClient.users.findUnique({
+            where : {
+                email : req.body.email
+            },
+            include : {
+                mahasiswa: true
+            }
+        });
+
+        if(!user) return res.status(404).json({message : "email tidak ditemukan"});
+
+        const match = await bycript.compare(req.body.password, user.password);
+        if(!match) return res.status(400).json({message : "password salah"});
+        const userId = user.id;
+        const name = user.mahasiswa[0].nama;
+        const email = user.mahasiswa[0].email;
+
+        const accessToken = jwt.sign({userId, name, email}, process.env.ACCES_TOKEN_SECRET, {expiresIn: '1d'});
+
+    } catch (error) {
+        res.status(404).json({message : "email tidak ditemukan"})
     }
 }
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
+import dotenv from "dotenv"
 import { getUsers } from '../controllers/users.js';
 import { Register } from '../controllers/users.js';
+dotenv.config()
 
 const router = express.Router();
 
