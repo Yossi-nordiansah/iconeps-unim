2c78c300d1ce2f73a023e213c7d8a8d1a1149dbd
Yossi-nordiansah <yossi.nordiansah99@gmail.com>
2024-12-15 23:43:52 +0700

    kirim respon ke client

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -13,34 +13,34 @@ export const getUsers = async (req, res) => {
 
 export const Register = async (req, res) => {
     const { username, password, confPassword, nama, nim, email, prodi, semester, nomor_telepon } = req.body;
-    if(password !== confPassword) return res.status(400).json({message : "password dan confirm password tidak cocok"});
+    if (password !== confPassword) return res.status(400).json({ message: "password dan confirm password tidak cocok" });
     const salt = await bycript.genSalt();
     const hashPassword = await bycript.hash(password, salt);
 
     try {
         const users = await prismaClient.users.create({
-            data : {
-                username : username,
-                password : hashPassword,
-                role : "mahasiswa"
+            data: {
+                username: username,
+                password: hashPassword,
+                role: "mahasiswa"
             }
         });
 
         await prismaClient.mahasiswa.create({
             data: {
-                user_id : users.id,
-                nama : nama,
-                nim : nim,
-                email : email,
-                prodi : prodi,
-                semester : semester,
-                nomor_telepon : nomor_telepon
+                user_id: users.id,
+                nama: nama,
+                nim: nim,
+                email: email,
+                prodi: prodi,
+                semester: semester,
+                nomor_telepon: nomor_telepon
             }
         })
 
-        res.status(201).send({message : "Registrasi Berhasil"});
+        res.status(201).send({ message: "Registrasi Berhasil" });
     } catch (error) {
-        res.status(500).send({message : 'Registrasi Gagal'})
+        res.status(500).send({ message: 'Registrasi Gagal' })
         console.log(error)
     }
 }
@@ -48,25 +48,39 @@ export const Register = async (req, res) => {
 export const Login = async (req, res) => {
     try {
         const user = await prismaClient.users.findUnique({
-            where : {
-                email : req.body.email
+            where: {
+                email: req.body.email
             },
-            include : {
+            include: {
                 mahasiswa: true
             }
         });
 
-        if(!user) return res.status(404).json({message : "email tidak ditemukan"});
+        if (!user) return res.status(404).json({ message: "email tidak ditemukan" });
 
         const match = await bycript.compare(req.body.password, user.password);
-        if(!match) return res.status(400).json({message : "password salah"});
+        if (!match) return res.status(400).json({ message: "password salah" });
         const userId = user.id;
         const name = user.mahasiswa[0].nama;
         const email = user.mahasiswa[0].email;
 
-        const accessToken = jwt.sign({userId, name, email}, process.env.ACCES_TOKEN_SECRET, {expiresIn: '1d'});
+        const accessToken = jwt.sign({ userId, name, email }, process.env.ACCES_TOKEN_SECRET, { expiresIn: '20s' });
+        const refreshToken = jwt.sign({ userId, name, email }, process.env.ACCES_REFRESH_SECRET, { expiresIn: '1d' });
 
+        await prismaClient.users.update({
+            refreshToken: refreshToken,
+            where: {
+                id: userId
+            }
+        });
+
+        res.cookie('refreshToken', refreshToken,{
+            httpOnly : true,
+            maxAge : 24 * 60 * 60 * 1000
+        });
+        
+        res.json({accessToken})
     } catch (error) {
-        res.status(404).json({message : "email tidak ditemukan"})
+        res.status(404).json({ message: "email tidak ditemukan" })
     }
 }
