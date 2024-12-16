de7208094dc4cb49ee9f7300bd0523b5884f7664
Yossi-nordiansah <yossi.nordiansah99@gmail.com>
2024-12-16 22:13:52 +0700

    proses login berhasil

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,11 +1,14 @@
 import prismaClient from "../src/prisma-client.js";
-import bycript from "bcrypt";
+import dotenv from 'dotenv'
+import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+
+dotenv.config();
 
 export const getUsers = async (req, res) => {
     try {
         const users = await prismaClient.users.findMany({});
-        res.json(users)
+        res.json(users);
     } catch (error) {
         console.log(error)
     }
@@ -14,8 +17,8 @@ export const getUsers = async (req, res) => {
 export const Register = async (req, res) => {
     const { username, password, confPassword, nama, nim, email, prodi, semester, nomor_telepon } = req.body;
     if (password !== confPassword) return res.status(400).json({ message: "password dan confirm password tidak cocok" });
-    const salt = await bycript.genSalt();
-    const hashPassword = await bycript.hash(password, salt);
+    const salt = await bcrypt.genSalt();
+    const hashPassword = await bcrypt.hash(password, salt);
 
     try {
         const users = await prismaClient.users.create({
@@ -47,40 +50,49 @@ export const Register = async (req, res) => {
 
 export const Login = async (req, res) => {
     try {
-        const user = await prismaClient.users.findUnique({
+        const user = await prismaClient.users.findFirst({
             where: {
-                email: req.body.email
+                mahasiswa: {
+                    some: {
+                        email: req.body.email
+                    }
+                }
             },
             include: {
                 mahasiswa: true
             }
         });
 
-        if (!user) return res.status(404).json({ message: "email tidak ditemukan" });
 
-        const match = await bycript.compare(req.body.password, user.password);
+        if (!user) {
+            console.log("Kondisi !user terpenuhi");
+            return res.status(404).json({ message: "email tidak ditemukan" });
+        }
+
+        const match = await bcrypt.compare(req.body.password, user.password);
         if (!match) return res.status(400).json({ message: "password salah" });
         const userId = user.id;
         const name = user.mahasiswa[0].nama;
         const email = user.mahasiswa[0].email;
 
-        const accessToken = jwt.sign({ userId, name, email }, process.env.ACCES_TOKEN_SECRET, { expiresIn: '20s' });
-        const refreshToken = jwt.sign({ userId, name, email }, process.env.ACCES_REFRESH_SECRET, { expiresIn: '1d' });
+        const accessToken = jwt.sign({ userId, name, email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '20s' });
+        const refreshToken = jwt.sign({ userId, name, email }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '1d' });
 
         await prismaClient.users.update({
-            refreshToken: refreshToken,
+            data: {
+                refresh_token: refreshToken
+            },
             where: {
                 id: userId
             }
         });
 
-        res.cookie('refreshToken', refreshToken,{
-            httpOnly : true,
-            maxAge : 24 * 60 * 60 * 1000
+        res.cookie('refreshToken', refreshToken, {
+            httpOnly: true,
+            maxAge: 24 * 60 * 60 * 1000
         });
-        
-        res.json({accessToken})
+        res.json({ accessToken, message : "login berhasil" })
     } catch (error) {
-        res.status(404).json({ message: "email tidak ditemukan" })
+        res.status(404).json({ message: "Terjadi kesalahan pada server" })
     }
 }
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,11 +2,13 @@ import express from 'express';
 import dotenv from "dotenv"
 import { getUsers } from '../controllers/users.js';
 import { Register } from '../controllers/users.js';
+import { Login } from '../controllers/users.js';
 dotenv.config()
 
 const router = express.Router();
 
 router.get('/users', getUsers);
 router.post('/users/register', Register);
+router.post("/users/login", Login)
 
 export default router;
