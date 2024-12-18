acdef875c5904faf8c7ff63e4ae2e12cace3539e
Yossi-nordiansah <yossi.nordiansah99@gmail.com>
2024-12-18 23:01:53 +0700

    menambahkan cross-origin recourse sharing

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import cookieParser from "cookie-parser";
 import router from './routes/index.js';
+import cors from 'cors'
 
 const app = express();
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }))
 app.use(cookieParser());
 app.use(express.json());
 
