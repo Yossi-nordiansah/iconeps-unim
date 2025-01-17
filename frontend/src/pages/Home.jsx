1f1a25cc735c29138d309f3b605f24a0a8e4a760
Yossi-nordiansah <yossi.nordiansah99@gmail.com>
2025-01-17 23:14:46 +0700

    perubahan pada kuisioner

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,16 +5,21 @@ import Keunggulan from '../components/Keunggulan'
 import BroadcastEmailForm from '../components/BroadcastEmailForm'
 import UploadExcel from '../components/UploadExcel'
 import Kuisioner from '../components/Kuisioner'
+import Login from '../components/Login'
+import Registrasi from './Registrasi'
 
 const Home = () => {
   return (
     <div>
         <Hero/>
         <Keunggulan/>
-        {/* <BroadcastEmailForm/> */}
+        <BroadcastEmailForm/>
         <IconepsIntro/>
-        {/* <UploadExcel/> */}
+        <UploadExcel/>
         <Kuisioner/>
+        <Login/>
+        <Registrasi/>
+        //bisa hibrid bisa online
     </div>
   )
 }
